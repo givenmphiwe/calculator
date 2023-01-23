0abb60c2e7044bd031c220a2a8ad65ef371dd0d5
Given Makhubela <giggslive52@gmail.com>
2023-01-23 14:25:53 +0200

    calc

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,5 +1,6 @@
 import 'package:calculator/main.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MyApp());
@@ -97,20 +98,94 @@ class _CalculatorState extends State<Calculator> {
   Widget CustomButton(String Text) {
     return InkWell(
       splashColor: Color(0xFF1d2630),
-      onTap: () {},
+      onTap: () {
+        setState(() {
+          handleButtons();
+        });
+      },
       child: Ink(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: getBgColor(text),
           borderRadius: BorderRadius.circular(10),
           boxShadow: [
             BoxShadow(
                 color: Colors.white.withOpacity(0.1),
                 blurRadius: 4,
                 spreadRadius: 0.5,
-                offset: Offset(-3, -3)),
+                offset: Offset(-3, -3)
+                ),
           ],
         ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: getColor(text),
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              ),
+            ),
+            ),
       ),
     );
+  }
+  getColor(String text){
+    if(text == "/" || text == "*" || text == "+" || text == "-" || text == "C" || text == "(" || text == ")" ){
+      return Color.fromARGB(255, 252, 100, 100)
+    }
+    
+    return Colors.white;
+  }
+
+  getBgColor(String text){
+    if(text == "=" ){
+      return Color.fromARGB(255, 252, 100, 100)
+    }
+    if(text == "=" ){
+      return Color.fromARGB(255, 104, 204, 159)
+    }
+    return Color(0xFF1d2630);
+  }
+
+  handleButtons(String text){
+    if (text =="AC"){
+      userInput = "";
+      result = "0";
+      return ;
+    }
+    if (text == "C"){
+      if (userInput.isNotEmpty){
+        userInput = userInput.substring(0, userInput.length -1);
+        return;
+      }
+      else{
+        return null;
+      }
+    }
+    if (text == "="){
+      result = calculator();
+      userInput = result;
+
+      if (result.endsWith(".0")){
+        result = result.replaceAll(".0", "");
+      }
+      if (result.endsWith(".0")){
+        result = result.replaceAll(".0", "");
+        return;
+      }
+    }
+
+    userInput = userInput + text;
+  }
+
+  String calculator(){
+    try{
+      var exp = Parser().parse(userInput);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    }catch(e){
+    return "Error";
+    }
+  
   }
 }
